b03cd9debf0713f64b5bc3f6433a43630fd38bb0
Nihal Kumar <77753226+nihxlkumar@users.noreply.github.com>
2024-08-01 23:49:01 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCtWar09Jcq8-9ZoobzoFw1ZWJIxZeGgC4",
-  authDomain: "indigo-hack-b1061.firebaseapp.com",
-  projectId: "indigo-hack-b1061",
-  storageBucket: "indigo-hack-b1061.appspot.com",
-  messagingSenderId: "396118267828",
-  appId: "1:396118267828:web:682c1007cc5ad2fcfb44e1",
-  measurementId: "G-LXWDZ9S4TB"
+  apiKey: "2-2",
+  authDomain: "indigo-hack-2.firebaseapp.com",
+  projectId: "indigo-hack-2",
+  storageBucket: "indigo-hack-2.appspot.com",
+  messagingSenderId: "2",
+  appId: "1:22:web:2",
+  measurementId: "G-2"
 };
 
 export const app = initializeApp(firebaseConfig);
